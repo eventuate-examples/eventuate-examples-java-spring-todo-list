a3c69fcadd9a4b97728bb1cb7d9d213d1da6fc0f
Chris Richardson <chris@chrisrichardson.net>
2015-12-21 14:32:17 -0800

    Upgraded to client framework 0.12 with new configuration http://eventuate.io/docs/java/spring-configuration.html Upgraded to Spring Boot 1.2.8 Misc changes to build scripts

--- build-and-test-all.sh
+++ build-and-test-all.sh
@@ -6,9 +6,21 @@ docker-compose rm -v --force
 
 docker-compose up -d mysql
 
-./gradlew clean build $*
+if [ -z "$DOCKER_HOST_IP" ] ; then
+  export DOCKER_HOST_IP=$(docker-machine ip default)
+  echo set DOCKER_HOST_IP $DOCKER_HOST_IP
+fi
+
+if [ -z "$SPRING_DATASOURCE_URL" ] ; then
+  export SPRING_DATASOURCE_URL=jdbc:mysql://${DOCKER_HOST_IP}:3307/es-test
+  echo Set SPRING_DATASOURCE_URL $SPRING_DATASOURCE_URL
+fi
+
+./gradlew $* build
 
 docker-compose up -d standaloneservice
+
+./gradlew --offline $* :e2etest:cleanTest :e2etest:testClasses
 
 echo -n waiting for service....
 
@@ -24,6 +36,10 @@ while [[ true ]]; do
         sleep 1
 done
 
-./gradlew :e2etest:cleanTest :e2etest:test $*
+set -e
 
+./gradlew --offline $* :e2etest:test
+
+docker-compose stop
+docker-compose rm -v --force
 
