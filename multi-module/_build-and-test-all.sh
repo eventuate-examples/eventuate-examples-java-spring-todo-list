e09bdd25850857e40d2db7b0f98a5218502e2209
Chris Richardson <chris@chrisrichardson.net>
2017-01-17 08:23:07 -0800

    Upgraded to latest Eventuate/Eventuate Local releases

--- multi-module/_build-and-test-all.sh
+++ multi-module/_build-and-test-all.sh
@@ -15,11 +15,11 @@ fi
 
 DOCKER_COMPOSE="docker-compose -p java-spring-todo-list"
 
-if [ "$1" = "-f" ] ; then
+while [ "$1" = "-f" ] ; do
   shift;
   DOCKER_COMPOSE="$DOCKER_COMPOSE -f ${1?}"
   shift
-fi
+done
 
 if [ "$1" = "--use-existing" ] ; then
   shift;
@@ -43,14 +43,14 @@ if [ -z "$SPRING_DATASOURCE_URL" ] ; then
   echo Set SPRING_DATASOURCE_URL $SPRING_DATASOURCE_URL
 fi
 
-./gradlew $* build -x :e2etest:test
+./gradlew $BUILD_AND_TEST_ALL_EXTRA_GRADLE_ARGS $* build -x :e2etest:test
 
 if [ -z "$EVENTUATE_LOCAL" ] && [ -z "$EVENTUATE_API_KEY_ID" -o -z "$EVENTUATE_API_KEY_SECRET" ] ; then
   echo You must set EVENTUATE_API_KEY_ID and  EVENTUATE_API_KEY_SECRET
   exit -1
 fi
 
-./gradlew --offline $* -P ignoreE2EFailures=false :e2etest:cleanTest :e2etest:testClasses
+./gradlew $BUILD_AND_TEST_ALL_EXTRA_GRADLE_ARGS --offline $* :e2etest:cleanTest :e2etest:testClasses
 
 ${DOCKER_COMPOSE?} build
 
@@ -58,7 +58,7 @@ ${DOCKER_COMPOSE?} up -d commandsideservice querysideservice
 
 ./wait-for-services.sh $DOCKER_HOST_IP 8081 8082
 
-./gradlew --offline $* :e2etest:cleanTest :e2etest:test
+./gradlew $BUILD_AND_TEST_ALL_EXTRA_GRADLE_ARGS --offline $* -P ignoreE2EFailures=false :e2etest:cleanTest :e2etest:test
 
 if [ $NO_RM = false ] ; then
   ${DOCKER_COMPOSE?} stop
