74d4949c42914f0deaae10e0b0e5d3c442c64762
Artem Sidorkin <Artem.Sidorkin@digitalarrowtech.com>
2017-11-07 17:15:22 +0300

    Fixing according the code review.

--- single-module/build-and-test-all-eventuate-local-postgres.sh
+++ single-module/build-and-test-all-eventuate-local-postgres.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 
-sh set-env-postgres.sh
-
-./gradlew clean
-./gradlew assemble -P eventuateDriver=local
+. set-env-postgres.sh
 
 docker-compose -f docker-compose-eventuate-local-postgres.yml down
+docker-compose -f docker-compose-eventuate-local-postgres.yml up --build -d cdcservice
+
+./gradlew assemble -P eventuateDriver=local
+#./gradlew -x e2eTest build -P eventuateDriver=local
+
 docker-compose -f docker-compose-eventuate-local-postgres.yml up --build -d
 
 ./wait-for-services.sh $DOCKER_HOST_IP 8080
 
-./gradlew $* cleanTest
-./gradlew $* e2eTest
+./gradlew e2eTest cleanTest e2eTest clean -P ignoreE2EFailures=false
+
+docker-compose -f docker-compose-eventuate-local-postgres.yml down
