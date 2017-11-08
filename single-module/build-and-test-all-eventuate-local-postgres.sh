96d0d09e6abfc1ec34631a82c654320379217885
Artem Sidorkin <Artem.Sidorkin@digitalarrowtech.com>
2017-11-08 10:15:38 +0300

    Fixing test scripts.

--- single-module/build-and-test-all-eventuate-local-postgres.sh
+++ single-module/build-and-test-all-eventuate-local-postgres.sh
@@ -5,13 +5,12 @@
 docker-compose -f docker-compose-eventuate-local-postgres.yml down
 docker-compose -f docker-compose-eventuate-local-postgres.yml up --build -d cdcservice
 
-./gradlew assemble -P eventuateDriver=local
-#./gradlew -x e2eTest build -P eventuateDriver=local
+./gradlew -x e2eTest build -P eventuateDriver=local
 
 docker-compose -f docker-compose-eventuate-local-postgres.yml up --build -d
 
 ./wait-for-services.sh $DOCKER_HOST_IP 8080
 
-./gradlew e2eTest cleanTest e2eTest clean -P ignoreE2EFailures=false
+./gradlew cleanTest e2eTest -P ignoreE2EFailures=false
 
 docker-compose -f docker-compose-eventuate-local-postgres.yml down
