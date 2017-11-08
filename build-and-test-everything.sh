26e3062f3f8211033ec0ceec4d64f45aef33cb82
Chris Richardson <chris@chrisrichardson.net>
2017-11-08 10:11:49 -0800

    Added new script

---
+++ build-and-test-everything.sh
@@ -0,0 +1,18 @@
+#! /bin/bash -e
+
+set -e
+set -o pipefail
+
+SCRIPTS="single-module/build-and-test-all-eventuate-local-postgres.sh"
+
+date > build-and-test-everything.log
+
+for script in $SCRIPTS ; do
+   echo '****************************************** Running' $script
+   date >> build-and-test-everything.log
+   echo '****************************************** Running' $script >> build-and-test-everything.log
+
+   (cd $(dirname $script) ; ./$(basename $script) ) | tee -a build-and-test-everything.log
+done
+
+echo 'Finished successfully!!!'
