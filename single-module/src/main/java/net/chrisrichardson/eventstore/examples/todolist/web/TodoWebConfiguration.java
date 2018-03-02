cf9a35694bdf04781069289a411aa60c8e789527
Artem Sidorkin <Artem.Sidorkin@digitalarrowtech.com>
2018-03-02 18:30:53 +0300

    Removing redundant configuration.

--- single-module/src/main/java/net/chrisrichardson/eventstore/examples/todolist/web/TodoWebConfiguration.java
+++ single-module/src/main/java/net/chrisrichardson/eventstore/examples/todolist/web/TodoWebConfiguration.java
@@ -1,32 +1,19 @@
 package net.chrisrichardson.eventstore.examples.todolist.web;
 
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
-import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.context.request.RequestContextListener;
-import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @ComponentScan("net.chrisrichardson.eventstore.examples.todolist.web")
 public class TodoWebConfiguration extends WebMvcConfigurerAdapter {
-
-    @Bean
-    public ServletListenerRegistrationBean<RequestContextListener> httpRequestContextListener() {
-        return new ServletListenerRegistrationBean<>(new RequestContextListener());
-    }
-
-    @Bean
-    public HttpMessageConverters customConverters() {
-        HttpMessageConverter<?> additional = new MappingJackson2HttpMessageConverter();
-        return new HttpMessageConverters(additional);
-    }
+  @Bean
+  public HttpMessageConverters customConverters() {
+    HttpMessageConverter<?> additional = new MappingJackson2HttpMessageConverter();
+    return new HttpMessageConverters(additional);
+  }
 }
