effedb614b632bba2c96251cbfa4e251a5b6a004
Chris Richardson <chris@chrisrichardson.net>
2018-03-27 08:58:21 -0700

    fixed scripts

--- build-and-test-everything.sh
+++ build-and-test-everything.sh
@@ -3,8 +3,13 @@
 set -e
 set -o pipefail
 
-SCRIPTS="single-module/build-and-test-all-eventuate-local-postgres.sh"
-
+SCRIPTS="single-module/build-and-test-all.sh
+single-module/build-and-test-all-eventuate-local.sh
+single-module/build-and-test-all-eventuate-local-postgres.sh
+multi-module/build-and-test-all.sh
+multi-module/build-and-test-all-eventuate-local.sh
+multi-module/build-and-test-all-eventuate-local-postgres.sh
+"
 date > build-and-test-everything.log
 
 for script in $SCRIPTS ; do
