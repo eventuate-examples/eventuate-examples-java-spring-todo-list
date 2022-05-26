2635f7332e04e61f3df2ce61e3df599bbc59b71d
Chris Richardson <chris@chrisrichardson.net>
2022-05-26 14:56:37 +0900

    Removed poinless gradlew --offline

--- _build-and-test-all.sh
+++ _build-and-test-all.sh
@@ -44,14 +44,14 @@ fi
 
 ./gradlew $BUILD_AND_TEST_ALL_EXTRA_GRADLE_ARGS $* build -x :e2etest:test
 
-./gradlew $BUILD_AND_TEST_ALL_EXTRA_GRADLE_ARGS --offline $* :e2etest:cleanTest :e2etest:testClasses
+./gradlew $BUILD_AND_TEST_ALL_EXTRA_GRADLE_ARGS $* :e2etest:cleanTest :e2etest:testClasses
 
 ./gradlew ${database}${mode}ComposeBuild
 ./gradlew -P dockerComposeNoCreate=true ${database}${mode}ComposeUp
 
 ./wait-for-services.sh $DOCKER_HOST_IP 8081 8082 $EXTRA_PORTS_TO_WAIT_FOR
 
-./gradlew $BUILD_AND_TEST_ALL_EXTRA_GRADLE_ARGS --offline $* -P ignoreE2EFailures=false :e2etest:cleanTest :e2etest:test
+./gradlew $BUILD_AND_TEST_ALL_EXTRA_GRADLE_ARGS $* -P ignoreE2EFailures=false :e2etest:cleanTest :e2etest:test
 
 if [ $NO_RM = false ] ; then
   ./gradlew ${database}${mode}ComposeDown
